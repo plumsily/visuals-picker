2e38468f6d0c041fff2c19c9eeab2349c415d55a
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-01-20 23:15:15 -0800

    added bpm animation toggle and slider and beat size

--- js/main.js
+++ js/main.js
@@ -2,12 +2,26 @@ document.getElementById("purple").onclick = partyPurple;
 document.getElementById("green").onclick = partyGreen;
 document.getElementById("blue").onclick = partyBlue;
 document.getElementById("yellow").onclick = partyYellow;
-document.getElementById("gif").onclick = partyGif;
-// document.getElementById("gif-base").style.display = "none";
+document.getElementById("bpm-switch").onclick = toggleBPM;
+document.getElementById("beat-small").onclick = setBeatSm;
+document.getElementById("beat-medium").onclick = setBeatMd;
+document.getElementById("beat-large").onclick = setBeatLg;
+
+let slider = document.getElementById("bpm-range");
+let output = document.getElementById("bpm-value");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  document.querySelector(".background").style.animationDuration = `${
+    1 / (this.value / 60)
+  }s`;
+};
 
 function partyPurple() {
   document.querySelector(".background").style.background =
-    "url(https://i.giphy.com/JpScsr53qh6sR6dSeJ.gif) no-repeat center center";
+    "url(https://i.giphy.com/Id6QAoJlekX3mkWIZj.gif) no-repeat center center";
   document.querySelector(".background").style.backgroundSize = "cover";
 
   // document.querySelector("body").style.backgroundColor = "rgba(241,63,247,1)";
@@ -24,16 +38,61 @@ function partyGreen() {
 }
 
 function partyBlue() {
-  document.querySelector("body").style.backgroundColor = "rgba(0,254,255)";
+  document.querySelector(".background").style.background =
+    "url(https://i.giphy.com/QqDXvbotoEvhEhFQ8H.gif) no-repeat center center";
+  document.querySelector(".background").style.backgroundSize = "cover";
+
+  //   document.querySelector("body").style.backgroundColor = "rgba(0,254,255)";
   document.querySelector("body").style.color = "white";
 }
 
 function partyYellow() {
-  document.querySelector("body").style.backgroundColor = "rgb(255, 254, 0)";
+  document.querySelector(".background").style.background =
+    "url(https://i.giphy.com/VJHGm6V1szmCTrGJSl.gif) no-repeat center center";
+  document.querySelector(".background").style.backgroundSize = "cover";
+
+  //   document.querySelector("body").style.backgroundColor = "rgb(255, 254, 0)";
   document.querySelector("body").style.color = "black";
 }
 
-function partyGif() {
-  document.getElementById("gif").style.display = "none";
-  document.querySelector("body").style.color = "black";
+function toggleBPM() {
+  if (
+    document.querySelector(".background").style.animationPlayState === "paused"
+  ) {
+    document.querySelector(".background").style.animationPlayState = "running";
+    document.getElementById("bpm-switch").style.backgroundColor =
+      "rgba(60,179,113,0.8)";
+  } else {
+    document.querySelector(".background").style.animationPlayState = "paused";
+    document.getElementById("bpm-switch").style.backgroundColor =
+      "rgba(60,179,113,0)";
+  }
+}
+
+function setBeatSm() {
+  document.querySelector(".background").style.animationName = "beatSm";
+  document.getElementById("beat-small").style.backgroundColor =
+    "rgba(60,179,113,0.8)";
+  document.getElementById("beat-medium").style.backgroundColor =
+    "rgba(60,179,113,0)";
+  document.getElementById("beat-large").style.backgroundColor =
+    "rgba(60,179,113,0)";
+}
+function setBeatMd() {
+  document.querySelector(".background").style.animationName = "beatMd";
+  document.getElementById("beat-small").style.backgroundColor =
+    "rgba(60,179,113,0)";
+  document.getElementById("beat-medium").style.backgroundColor =
+    "rgba(60,179,113,0.8)";
+  document.getElementById("beat-large").style.backgroundColor =
+    "rgba(60,179,113,0)";
+}
+function setBeatLg() {
+  document.querySelector(".background").style.animationName = "beatLg";
+  document.getElementById("beat-small").style.backgroundColor =
+    "rgba(60,179,113,0)";
+  document.getElementById("beat-medium").style.backgroundColor =
+    "rgba(60,179,113,0)";
+  document.getElementById("beat-large").style.backgroundColor =
+    "rgba(60,179,113,0.8)";
 }
